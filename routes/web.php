145e76277e6d8cf547b8f560e40de18fd76279ab
robihulmufid <robihul_01@yahoo.com>
2025-07-10 02:52:02 +0700

    menambahkan crud product

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+//route resource for products
+Route::resource('/products', ProductController::class);
 
 Route::get('/', function () {
     return view('welcome');
